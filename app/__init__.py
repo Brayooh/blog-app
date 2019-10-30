3ceaad16899a82680ecd17edc316fb3dafdd1953
brayooh <brianndichu.bn@gmail.com>
2019-10-30 12:43:03 +0300

    modify login manager

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_bootstrap import Bootstrap
 from flask_login import LoginManager
 from flask_mail import Mail
 from flask_sqlalchemy import SQLAlchemy
-from config import Config
+from config import config_options
 
 
 db = SQLAlchemy()
