558dff545e931f41b808478f66bbdc50edb5b6a8
brayooh <brianndichu.bn@gmail.com>
2019-10-30 06:45:00 +0300

    add email configurations + set secret key

---
+++ config.py
@@ -0,0 +1,22 @@
+import os
+
+from dotenv import load_dotenv as ld
+
+ld()
+
+
+class Config:
+    debug = True
+    # SQLALCHEMY_DATABASE_URI = os.environ.get('DATABASE_URL')
+    SQLALCHEMY_DATABASE_URI = 'postgresql+psycopg2://brayooh:brian@localhost/blog'
+    SECRET_KEY = 'qwertyuiop'
+    MAIL_USERNAME = os.environ.get('MAIL_USERNAME')
+    MAIL_PASSWORD = os.environ.get('MAIL_PASSWORD')
+    SENDGRID_API_KEY=os.environ.get('SENDGRID_API_KEY')
+    DEFAULT_SENDGRID_SENDER =  os.environ.get('DEFAULT_SENDGRID_SENDER')
+
+    #  email configurations
+    MAIL_SERVER = 'smtp.gmail.com'
+    MAIL_PORT = 465
+    MAIL_USE_TLS = False
+    MAIL_USE_SSL = True
