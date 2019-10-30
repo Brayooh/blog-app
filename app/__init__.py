495148109fcc1d089f9cbd0a9078625d6e71f449
brayooh <brianndichu.bn@gmail.com>
2019-10-30 12:37:01 +0300

    modify login manager

--- app/__init__.py
+++ app/__init__.py
@@ -8,7 +8,7 @@ from config import Config
 
 db = SQLAlchemy()
 login_manager = LoginManager()
-login_manager.init_app(app)
+
 bootstrap = Bootstrap()
 mail = Mail()
 login_manager.login_view = 'auth.login'
@@ -20,6 +20,7 @@ def create_app(Config_name):
     app = Flask(__name__)
 
     app.config.from_object(Config_options[Config_name])
+    login_manager.init_app(app)
     from .auth import auth as auth_blueprint
     from .main import main as main_blueprint
     app.register_blueprint(auth_blueprint)
