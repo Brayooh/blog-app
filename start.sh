b801f47d1cf6040777d0fd909fe3e74123701314
brayooh <brianndichu.bn@gmail.com>
2019-10-30 16:39:39 +0300

    add modifications

--- start.sh
+++ start.sh
@@ -3,6 +3,6 @@ export SECRET_KEY='qwertyuiop'
 export MAIL_USERNAME='brianndichu.bn@gmail.com'
 
 export MAIL_PASSWORD='brayooh'
-export DATABASE_URL='postgresql+psycopg2://brayooh:brian@localhost/blog'
+export SQLALCHEMY_DATABASE_URI='postgresql+psycopg2://brayooh:brian@localhost/blog'
 
 python3.6 manage.py server
